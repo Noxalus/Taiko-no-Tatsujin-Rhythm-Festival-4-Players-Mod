fb1c520f47d2a5b816e37b0ba8012351642e7935
Jonathan Jarri <jonathan@homoludens.games>
2025-01-05 15:23:02 +0100

    Create base solution + .gitignore file.

---
+++ TnTRF-4-Players-Mod/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using MelonLoader;
+
+[assembly: MelonInfo(typeof(TnTRF_4_Players_Mod.Main), "TnTRF 4 Players Mod", "0.0.1", "Noxalus")]
+[assembly: MelonGame("BANDAI NAMCO Entertainment Inc.", "Taiko no Tatsujin Rhythm Festival")]
---
+++ TnTRF-4-Players-Mod/Main.cs
@@ -0,0 +1,16 @@
+﻿using MelonLoader;
+
+namespace TnTRF_4_Players_Mod
+{
+    public class Main : MelonMod
+    {
+        public static Main Instance { get; private set; }
+
+        public override void OnInitializeMelon()
+        {
+            base.OnInitializeMelon();
+            Instance = this;
+            LoggerInstance.Msg("TnTRF 4 Players Mod has started!");
+        }
+    }
+}
